32b419eff489dce4d5ae2425570d58afef1141c0
zkq <imzhoukunqiang@gmail.com>
2023-10-11 14:06:57 +0000

    update

--- app/store/config.ts
+++ app/store/config.ts
@@ -34,7 +34,7 @@ export const DEFAULT_CONFIG = {
   theme: Theme.Auto as Theme,
   tightBorder: !!getClientConfig()?.isApp,
   sendPreviewBubble: true,
-  enableAutoGenerateTitle: true,
+  enableAutoGenerateTitle: false,
   sidebarWidth: DEFAULT_SIDEBAR_WIDTH,
 
   disablePromptHint: false,
@@ -162,7 +162,7 @@ export const useAppConfig = createPersistStore(
       }
 
       if (version < 3.7) {
-        state.enableAutoGenerateTitle = true;
+        state.enableAutoGenerateTitle = false;
       }
 
       if (version < 3.8) {
